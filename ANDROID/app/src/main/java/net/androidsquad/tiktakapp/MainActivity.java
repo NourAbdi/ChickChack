4bb7351a689f311d8ba5dfb5ef58bd4a3dc10188
NourAbdi <Dr.nour.3abdi@gmail.com>
2023-03-22 13:14:25 +0200

    DB linking

--- ANDROID/app/src/main/java/net/androidsquad/tiktakapp/MainActivity.java
+++ ANDROID/app/src/main/java/net/androidsquad/tiktakapp/MainActivity.java
@@ -1,14 +1,46 @@
 package net.androidsquad.tiktakapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseFirestore firestore ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firestore = FirebaseFirestore.getInstance();
+
+        Map<String,Object> user = new HashMap<>();
+        user.put("userName","Nour Abdi" );
+        user.put("phoneNumber","0525473646" );
+        user.put("password","Nour" );
+        user.put("mail","nour.mail" );
+
+        firestore.collection("user").add(user).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Toast.makeText(getApplicationContext(),"SUCCESS",Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),"FAILED",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
