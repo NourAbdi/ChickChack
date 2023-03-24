bec9bc3c2c932c23709b9395a6a8e2b01d1fec91
SalehSalsabeel <salehsalsabeel99@gmail.com>
2023-03-24 17:33:59 +0300

    adding
    headers and styles

--- chickChack/src/components/Headers.js
+++ chickChack/src/components/Headers.js
@@ -1,11 +1,20 @@
 import React from "react";
 
 import { View,Text,StyleSheet,Dimensions } from "react-native";
+import { colors,parameters } from "../global/styles";
 
 export default function Header(){
 
-    return (<View>
+    return (<View style = {styles.header}>
 
             </View>
     )
 }
+
+const styles= StyleSheet.create({
+   header:{
+        flexDireaction:"row",
+        backgroundColor:colors.buttons,
+        height:parameters.headerHeight
+   } 
+})
