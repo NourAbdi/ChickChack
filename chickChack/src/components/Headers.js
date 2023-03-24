b08a5712268fd86c5b1df80a6f1fef0d5f2d4a7a
NourAbdi <Dr.nour.3abdi@gmail.com>
2023-03-24 15:46:53 +0300

    sadsdas

---
+++ chickChack/src/components/Headers.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { View,Text,StyleSheet,Dimensions } from "react-native";
+
+export default function Header(){
+
+    return (<View>
+
+            </View>
+    )
+}
