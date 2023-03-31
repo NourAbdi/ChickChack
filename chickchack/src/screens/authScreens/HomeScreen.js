5b1fbd4418ee56bfe550cd62e71cc2a54f2375f8
SalehSalsabeel <salehsalsabeel99@gmail.com>
2023-03-31 05:35:54 +0300

    part 8
    until 30

--- chickchack/src/screens/authScreens/HomeScreen.js
+++ chickchack/src/screens/authScreens/HomeScreen.js
@@ -1,17 +1,72 @@
-import React from "react";
+import React,{useState} from "react";
 
-import { View,Text, StyleSheet } from 'react-native';
+import { View,Text, StyleSheet,TouchableOpacity,ScrollView} from 'react-native';
 import {Icon} from '@rneui/themed';
 import HomeHeader from "../../components/HomeHeader";
+import {colors,parameters} from "../../global/styles";
 
 
 export default function HomeScreen(){
+
+    const [delivery,setDelivery]= useState(true)
     return(
         <View style={styles.container}>
             <HomeHeader />
+            
+            <ScrollView
+                stickyHeaderIndices={[0]}
+                showsHorizontalScrollIndicator={true}
+            >
+                <View>
+                    <View style={{marginTop:5,flexDirection:'row',justifyContent:"space-evenly"}}>
+                        <TouchableOpacity
+                            onPress={()=>{
+                                setDelivery(true)
+                            }}
+                        >
+                            <View style={{...styles.deliveryButton,backgroundColor:delivery? colors.buttons:colors.grey5}}>
+                                <Text style={styles.deliveryText}>Delivery</Text>
+                            </View>
+                        </TouchableOpacity>
 
+                        <TouchableOpacity
+                            onPress={()=>{
+                                setDelivery(false)
+                            }}
+                        >
+                            <View style={{...styles.deliveryButton,backgroundColor:delivery? colors.grey5:colors.buttons}}>
+                                <Text style={styles.deliveryText}>Pick Up</Text>
+                            </View>
+                        </TouchableOpacity> 
+                    </View>
+                </View>
+
+                <View>
+                    <View style={{flexDirection:"row",alignItems:"center"}}>
+                        <Icon
+                            type = "material-community"
+                            name = "map-marker"
+                            color = {colors.grey1}
+                            size = {26}
+                        />
+                        
+                        <Text style={{marginLeft:5}}>22 Beessie Street</Text>
+                    </View>
+
+                    <View style={{flexDirection:"row",alignItems:"center"}}>
+                        <Icon
+                            type = "material-community"
+                            name = "clock-time-four"
+                            color = {colors.grey1}
+                            size = {26}
+                        />
+                        
+                        <Text style={{marginLeft:5}}>Now</Text>
+                    </View>
+                </View>
+
+            </ScrollView>
         </View>
-
     )
 }
 const styles= StyleSheet.create({
@@ -19,5 +74,15 @@ const styles= StyleSheet.create({
     container:{
         flex:1,
         
+    },
+    deliveryButton:{
+        paddingHorizontal:20,
+        borderRadius:15,
+        paddingVertical:5
+    },
+
+    deliveryText:{
+        marginLeft:5,
+        fontSize:16
     }
 })
