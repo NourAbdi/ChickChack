d2ea655ff2117c84d58dba68f0001557f89afcce
NourAbdi <Dr.nour.3abdi@gmail.com>
2023-04-02 02:41:12 +0300

    sdas

---
+++ chickchack/src/global/Data.js
@@ -0,0 +1,6 @@
+export const fillerData  = [
+    {name :"Fast food",image :require('../assets/Seafood.png'),id : "0"},
+    {name :"Burgers",image :require('../assets/Seafood.png'),id : "1"},
+    {name :"Salads",image :require('../assets/Seafood.png'),id : "2"},
+    {name :"Hotdog",image :require('../assets/Seafood.png'),id : "3"},
+    {name :"Seafood",image :require('../assets/Seafood.png'),id : "4"}];
