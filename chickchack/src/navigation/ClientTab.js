cee2637c3020d8f84d8b83455e0e5f42b628a390
SalehSalsabeel <salehsalsabeel99@gmail.com>
2023-04-05 02:35:53 +0300

    part 11 done

--- chickchack/src/navigation/ClientTab.js
+++ chickchack/src/navigation/ClientTab.js
@@ -4,11 +4,13 @@ import React from "react";
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import {Icon,withBadge} from '@rneui/themed';
 
+import{colors} from '../global/styles'
+import HomeScreen from "../screens/HomeScreen";
+import SearchScreen from "../screens/SearchScreen";
+import MyOrdersScreen from "../screens/MyOrdersScreen";
+import MyAccountScreen from "../screens/MyAccountScreen";
 
 import { View,Text, StyleSheet } from 'react-native';
-import{colors,parameters} from '../global/styles'
-import { create } from "react-test-renderer";
-import HomeScreen from "../screens/HomeScreen";
  
 
 const ClientTabs = createBottomTabNavigator();
@@ -17,21 +19,26 @@ export default function RootClientTabs(){
     
     return(
         <ClientTabs.Navigator
-            tabBarOption ={{
-                activeTintColor: colors.lightgreen
-            }}
-        
+
+        screenOptions={({ route }) => ({
+            tabBarInactiveTintColor: colors.grey3,
+            tabBarActiveTintColor:  colors.buttons
+        })}
+            // tabBarOptions ={{
+            //     activeTintColor: colors.buttons
+            // }}
         >
             <ClientTabs.Screen
                 name="HomeScreen"
                 component={HomeScreen}
                 options ={ 
                     {
-                        tabBarLabel : 'Home',
+                        tabBarLabel : "Home",
+                        headerShown:false,
                         tabBarIcon: ({color,size})=>(
                             <Icon
-                                name ="home"
-                                type="material"
+                                name ='home'
+                                type='material'
                                 color={color}
                                 size={size}
                             />
@@ -40,6 +47,68 @@ export default function RootClientTabs(){
                     }
                 }
             />
+
+            <ClientTabs.Screen
+                name="SearchScreen"
+                component={SearchScreen}
+                options ={ 
+                    {
+                        tabBarLabel : "Search",
+                        headerShown:false,
+                        tabBarIcon: ({color,size})=>(
+                            <Icon
+                                name ='search'
+                                type='material'
+                                color={color}
+                                size={size}
+                            />
+                        )
+                    
+                    }
+                }
+            />
+
+            <ClientTabs.Screen
+                name="MyOrdersScreen"
+                component={MyOrdersScreen}
+                options ={ 
+                    {
+                        tabBarLabel : "My Orders",
+                        headerShown:false,
+                        tabBarIcon: ({color,size})=>(
+                            <Icon
+                                name ='view-list'
+                                type='material'
+                                color={color}
+                                size={size}
+                            />
+                        )
+                    
+                    }
+                }
+            />
+
+            <ClientTabs.Screen
+                name="MyAccountScreen"
+                component={MyAccountScreen}
+                options ={ 
+                    {
+                        tabBarLabel : "My Account",
+                        headerShown:false,
+                        tabBarIcon: ({color,size})=>(
+                            <Icon
+                                name ='person'
+                                type='material'
+                                color={color}
+                                size={size}
+                            />
+                        )
+                    
+                    }
+                }
+            />
+
+
         </ClientTabs.Navigator>
     )
 }
