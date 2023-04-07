708470682bd277f98b5770ea5f19617f42d6441d
NourAbdi <Dr.nour.3abdi@gmail.com>
2023-04-07 23:32:10 +0300

    xzc
    xzc

---
+++ MealsToGo-master/App.js
@@ -0,0 +1,52 @@
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { ThemeProvider } from "styled-components/native";
+import { initializeApp } from "firebase/app";
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme";
+import { Navigation } from "./src/infrastructure/navigation";
+
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAaIrmc6nAn7yKCDUfj74JKaQodXCp4O08",
+  authDomain: "chickchack-acd3a.firebaseapp.com",
+  projectId: "chickchack-acd3a",
+  storageBucket: "chickchack-acd3a.appspot.com",
+  messagingSenderId: "983512532302",
+  appId: "1:983512532302:web:9baf13a1c98a33fa168a1f",
+  measurementId: "G-TGZ3771DLT"
+};
+
+initializeApp(firebaseConfig);
+
+export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
