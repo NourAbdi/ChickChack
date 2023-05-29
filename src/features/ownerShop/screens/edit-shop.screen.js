c57667c9a38c989a85235b7149ed334beb2f217b
SalehSalsabeel <salehsalsabeel99@gmail.com>
2023-05-29 15:29:18 +0300

    aa

--- src/features/ownerShop/screens/edit-shop.screen.js
+++ src/features/ownerShop/screens/edit-shop.screen.js
@@ -1,35 +1,17 @@
 import React, { useContext, useState,useRef } from "react";
-import { View,Animated,Switch,Text } from "react-native";
-import CheckBox from '@react-native-community/checkbox';
-
-
-import DropdownPicker from 'react-native-dropdown-picker';
-import ModalDropdown from 'react-native-modal-dropdown';
-import ModalSelector from 'react-native-modal-selector';
-
-
-import { Card } from "react-native-paper";
-
+import { View,Animated} from "react-native";
 
 import { OwnerShopContext } from "../../../services/ownerShop/ownerShop.context";
 import { colors } from "../../../infrastructure/theme/colors";
-
 import{
     HeaderImage,
-    AnimatedImageView,
-    AnimatedHeaderView,
     AnimatedScrollView,
     LoadingContainer,
     Loading,
-    ShopIcon,
-    MarginTop,
- } from '../components/shop-details.screen.style'
+} from '../components/shop-details.screen.style'
 
 import{
-    headerTranslate,
-    titleScale,
-    titleTranslate,
-    titleOpacity,
+  PrintHeader
 } from "../components/shop-details.screen.components"
 
 import {
@@ -37,7 +19,7 @@ import {
 } from "../components/edit-shop.screen.component"
 
 export const EditShopScreen = () => {
-  const { shop,updateShop } = useContext(OwnerShopContext);
+  const { shop,updateShop, isLoading } = useContext(OwnerShopContext);
   const [isTemporaryClose, setTemporaryClose] = useState(shop.IstemporaryClose);
   const scrollY = useRef(new Animated.Value(0)).current;
   const workingHours = shop.workingHours;
@@ -47,9 +29,16 @@ export const EditShopScreen = () => {
   //   // TODO: Implement the saving logic here
   //   updateShop( workingHours, isOpen );
   // };
+  // console.log("workingHoursworkingHoursworkingHoursworkingHoursworkingHoursworkingHours11",workingHours);
   
-  
-  console.log("workingHoursworkingHoursworkingHoursworkingHoursworkingHoursworkingHours11",workingHours);
+  if (isLoading) {
+    // If shop details are not yet fetched, you can show a loading indicator
+    return(
+      <LoadingContainer>
+        <Loading size={50} color={colors.mainblue} animating={true} />
+      </LoadingContainer>
+    );
+  }
   return (
     <View style={{ flex: 1 }}>
       <AnimatedScrollView scrollY={scrollY}>
@@ -57,15 +46,8 @@ export const EditShopScreen = () => {
         {PrintWorkingHours(workingHours,isTemporaryClose,setTemporaryClose)}
         
       </AnimatedScrollView>
+      {PrintHeader(shop.icon,scrollY)}
 
-      <AnimatedImageView style={{ opacity: titleOpacity(scrollY),transform: [{ translateY: headerTranslate(scrollY) }],}}>
-      </AnimatedImageView>
-
-      <AnimatedHeaderView style={[{ opacity: titleOpacity(scrollY), transform: [{ scale: titleScale(scrollY) }, { translateY: titleTranslate(scrollY) }],},]} >
-        <MarginTop>
-          <ShopIcon source={{ uri: shop.icon }} />
-        </MarginTop>
-      </AnimatedHeaderView>
 
       {/* <Button title="Save" onPress={handleSave()} /> */}
       
