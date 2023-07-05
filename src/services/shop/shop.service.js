e782a346fb32ec78ede1bfd43640bc7fbdef55d9
Nour Abdi <34486030+NourAbdi@users.noreply.github.com>
2023-07-05 13:39:35 +0300

    dasf
    df

--- src/services/shop/shop.service.js
+++ src/services/shop/shop.service.js
@@ -1,15 +1,30 @@
-import { host } from "../../utils/env";
+import { db } from "../../utils/env";
+import { collection, getDocs, query, where, doc, getDoc } from "firebase/firestore";
 
 export const getShopMenuByShopUid = async (shopUid) => {
   try {
-    const response = await fetch(`${host}/getShopMenuByShopUid?shopUid=${shopUid}`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch menu.");
+    const shopRef = doc(db, "shops", shopUid);
+    const shopDoc = await getDoc(shopRef);
+
+    if (shopDoc.exists()) {
+      const shopData = shopDoc.data();
+      const menu = shopData.menu || [];
+
+      const itemsCollection = collection(db, "items"); // Replace "items" with the actual collection name in your Firestore
+      const q = query(itemsCollection, where("itemUid", "in", menu));
+      const querySnapshot = await getDocs(q);
+
+      const menuItems = [];
+      querySnapshot.forEach((doc) => {
+        menuItems.push(doc.data());
+      });
+
+      return menuItems;
+    } else {
+      throw new Error("Shop not found.");
     }
-    const menuData = await response.json();
-    return menuData;
   } catch (error) {
-    console.error("Error fetching menu:", error);
-    return null; 
+    console.error("Error fetching shop menu:", error);
+    return null;
   }
 };
--- src/services/shops/shops.service.js
+++ src/services/shops/shops.service.js
@@ -1,13 +1,25 @@
-import { host } from "../../utils/env";
+import { db } from "../../utils/env";
+import {
+    collection,
+    getDocs,
+    query,
+    where,
+} from "firebase/firestore";
 
 export const getShopsByCityName = async (cityName) => {
   try {
-    const response = await fetch(`${host}/getShopsByCityName?cityName=${cityName}`);
-    if (!response.ok) {
-      throw new Error("Failed to fetch shops.");
-    }
-    const data = await response.json();
-    return data;
+    const shopCollection = collection(db, "shops"); // Replace "shops" with the actual collection name in your Firestore
+    
+    const q = query(shopCollection, where("city", "==", cityName)); // Assuming you have a "city" field in each shop document
+    
+    const querySnapshot = await getDocs(q);
+    
+    const shops = [];
+    querySnapshot.forEach((doc) => {
+      shops.push(doc.data());
+    });
+    
+    return shops;
   } catch (error) {
     throw new Error("Failed to fetch shops.");
   }
