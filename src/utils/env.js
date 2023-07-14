87253136fd14638824f5db6940b23b7bdc06be08
Nour Abdi <34486030+NourAbdi@users.noreply.github.com>
2023-07-14 14:35:03 +0300

    dsa

--- src/utils/env.js
+++ src/utils/env.js
@@ -4,7 +4,7 @@ const liveHost = "https://us-central1-chickchack-3069a.cloudfunctions.net";
 const localHost = "https://localhost:5001/chickchack-3069a/us-central1";
 
 export const isDevelopment = process.env.NODE_ENV === "development";
-export const isMock = true;
+
 // export const host = !isDevelopment || isAndroid ? liveHost : localHost;
 export const host = liveHost;
 
