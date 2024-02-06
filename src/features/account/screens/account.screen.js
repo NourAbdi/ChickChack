d4ab7d032fe4e77d165253275c8b3b0ba795790f
Nour Abdi <34486030+NourAbdi@users.noreply.github.com>
2024-02-06 13:48:18 +0200

    1231
    213

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,32 +1,12 @@
 import React, { useContext, useRef } from "react";
 import LottieView from "lottie-react-native";
-import { SafeAreaView, StatusBar, StyleSheet, View, Text, TouchableOpacity, Button, TextInput } from "react-native";
+import { View } from "react-native";
 import { FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha';
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
 import { useTranslation } from "react-i18next";
-
 import { app } from "../../../utils/env";
-import {
-  AccountBackground1,
-  AccountContainer1,
-  AccountCover1,
-  AuthButton,
-  Title,
-  AnimationWrapper1,
-  AnimationWrapper2,
-  LeftTitle,
-  RightTitle,
-  VerificationInput,
-  BodyText,
-  ButtonView,
-  ButtonText,
-} from "../components/account.styles";
-import {
-  GetHeader,
-  StatusBarPlaceHolder,
-
-} from "../components/account.component"
-
+import { VerificationInput, BodyText, ButtonView, ButtonText } from "../components/account.styles";
+import { GetHeader, StatusBarPlaceHolder } from "../components/account.component"
 import { Dimensions } from "react-native";
 
 const screenWidth = Dimensions.get("window").width;
@@ -36,6 +16,28 @@ export const AccountScreen = () => {
   const recaptchaVerifier = useRef(null);
   const { phoneNumber, setPhoneNumber, verificationId, setVerificationCode, sendVerificationCode, confirmVerificationCode } = useContext(AuthenticationContext);
   const { t, i18n } = useTranslation();
+
+  // Function to normalize phone number
+  const normalizePhoneNumber = (input) => {
+    // Remove all non-numeric characters
+    const numericInput = input.replace(/\D/g, "");
+    // Check if the input starts with "0" or "05"
+    if (numericInput.startsWith("0")) {
+      // If it starts with "0", replace with "+972"
+      return `+972${numericInput.slice(1)}`;
+    } else if (numericInput.startsWith("05")) {
+      // If it starts with "05", prepend with "+972"
+      return `+972${numericInput.slice(2)}`;
+    }
+    // Otherwise, return input as is
+    return input;
+  };
+
+  // Function to handle phone number change
+  const handlePhoneNumberChange = (input) => {
+    const normalizedNumber = normalizePhoneNumber(input);
+    setPhoneNumber(normalizedNumber);
+  };
 
   return (
     <View style={{ flex: 1 }}   >
@@ -53,7 +55,7 @@ export const AccountScreen = () => {
           autoCompleteType="tel"
           keyboardType="phone-pad"
           textContentType="telephoneNumber"
-          onChangeText={setPhoneNumber}
+          onChangeText={handlePhoneNumberChange}
         />
         <ButtonView onPress={() => sendVerificationCode(recaptchaVerifier)} disabled={!phoneNumber} isSelected={!phoneNumber} >
           <ButtonText>{t("Send Verification Code")}</ButtonText>
